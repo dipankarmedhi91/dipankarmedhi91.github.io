a6574fa59b99bbb91955998983a608161c650bff
DipankarMedhi <daps_tg@rediff.com>
2023-11-09 22:23:31 -0500

    asset prefixed

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
-// const repo = "dipankarmedhi91.github.io";
+const repo = "dipankarmedhi91.github.io";
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = ''
 let basePath = ''
 
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
+// if (isGithubActions) {
   assetPrefix = `/${repo}/`
   basePath = `/${repo}`
-}
+// }
 // const assetPrefix = `/${repo}/`;
 // const basePath = `/${repo}`;
 
