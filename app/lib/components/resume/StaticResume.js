e5731c9e93b876a97189820cc21643fb9bb6db51
DipankarMedhi <daps_tg@rediff.com>
2024-08-27 17:19:09 -0400

    resume updated

--- app/lib/components/resume/StaticResume.js
+++ app/lib/components/resume/StaticResume.js
@@ -9,58 +9,70 @@ const StaticResume = () => {
       <section>
         <p className={styles.title}>In Brief</p>
         <p className={styles.content}>
-          A driven, collaborative and versatile developer with {getExperience()}{" "}
-          of agile approach fullstack web development experience
+          A Full Stack Software Engineer with about {getExperience()} of
+          experience in both frontend and backend development along with
+          developing hybrid mobile applications.
           <br />
-          Passion for learning new technologies, seeking out challenges and to
-          keep improving constantly
-          <br />I thrive as a part of a team where we can have discussions and
-          debates on technologies/approaches/architectures
+          Successfully led cross-functional teams, mentored junior developers
+          while driving timely delivery of high-impact projects.
+          <br />I thrive as a part of a team where we can have explore and
+          discuss about technologies, tools and architectures
         </p>
       </section>
       <section>
         <p className={styles.title}>Skills</p>
         <section className={styles.content}>
-          <h2>Primary:-</h2>
-          JavaScript(ES6+), Node.js, Express, ReactJS, Ionic, Ext JS, MongoDB, HTML,
-          Sass, Git, Web Components, redis, Socket.io, mocha/jest, Apache kafka,
-          webpack, Azure cloud services
-          <h2>Secondary:-</h2>
-          Vue.js, Typescript, NextJs, GraphQL, docker, gitlab CI-CD pipelines, protobuf, PWA, 
-          gRPC, Grafana, AWS S3, .NET core, Java, TICK Stack, Apache Solr, FormIO
-          <h2>Tools:-</h2>
-          VSCode, IntelliJ, Jetbrains Rider, Mongo Compass, Docker Desktop,
-          Fiddler, Postman, cURL, PuTTY, SOAP UI
+          <h2>Frontend:</h2>
+          JavaScript, TypeScript, React, Vue.js, ExtJS, Web Components, Angular,
+          SASS, OAuth2
+          <h2>Backend:</h2>
+          Node.js, Express, NestJS, GraphQL, WebSocket, Redis, MongoDB, Kafka,
+          .NET Core
+          <h2>DevOps & Cloud:</h2>
+          Docker, Traefik, Azure Cloud Services (Cosmos DB, APIM, AD B2C, App
+          Insights, Notification Hub), AWS S3, Gitlab pipelines
+          <h2>Mobile & Hybrid Apps:</h2>
+          Ionic, Capacitor, Biometrics, Push Notifications, PWA, Kochava, Braze
+          <h2>Other Technologies:</h2>
+          Git, Apache Solr, Puppeteer, reCAPTCHA v3, SFTP, TICK Stack, Grafana,
+          Form.io
+          <h2>Methodologies:</h2>
+          Agile, TDD/BDD, Continuous Integration/Continuous Deployment (CI/CD)
         </section>
       </section>
       <section>
         <p className={styles.title}>Work Experience</p>
         <section className={styles.content}>
-        <h2>Company: </h2>
+          <h2>Company: </h2>
           <p>Globallogic Inc., May 2021 to present</p>
           <br />
           <h2>Roles: </h2>
           <p className={styles.roleName}>
             <em>Lead Software Engineer, Feb 2023 to present</em>{" "}
           </p>
-          Working with a major healthcare provider to develop a novel system to allow business teams to design, create and
-          deploy new services to customers in real time with very minimal interaction with IT team hence leading to high cost
-          savings and faster time to market, resposibilities include;
+          Working with a major healthcare provider to develop a novel system to
+          allow business teams to design, create and deploy new services to
+          customers in real time with very minimal interaction with IT team
+          hence leading to high cost savings and faster time to market,
+          resposibilities include;
           <ul>
             <li>
-            Work on RnD and create various PoC&apos;s on different tools and technologies that can help us achieve the target state
+              Work with Product team, designers, business team & architects to
+              discuss & design initial solution diagram and create initial
+              repository, pipeline and others with DevOps team
             </li>
             <li>
-            Work with Product team, designers, business team & architects to discuss & design initial solution diagram
-            and create initial repository, pipeline and other artifacts with DevOps team
+            Designed the core functionality using Form.io for authoring assessments and 
+            Web Components for isolated rendering which streamlined business processes leading to faster time to market and cost savings
             </li>
             <li>
-            Implement foundational components to help deliver user stories, help resource team to evaluate & onboard
-            new team members and create a smoother onboarding experience to ensure quick turnaround of deliverables
+              Work on RnD and create various PoC&apos;s on different tools and
+              technologies that can help us achieve the target state
             </li>
             <li>
-              Technologies: Angular 9, Sass, FormIO, Web Components, Nodejs, PingID (OAuth2), Azure cloud services
-              (Cosmos/APIM/App Insights/Storage/etc), BPM tools, etc
+              Technologies: Angular 9, Sass, Form.io, Web Components, Nodejs,
+              PingID (OAuth2), Azure cloud services (Cosmos/APIM/App
+              Insights/Storage/etc), BPM tools, etc
             </li>
           </ul>
           <br />
@@ -68,32 +80,41 @@ const StaticResume = () => {
             {" "}
             <em>Senior Software Engineer, July 2017 to June 2019</em>{" "}
           </p>
-          <p>Worked with a major financial client to develop a new hybrid web app to accelerate business to delivery speed of
-          Application to Android, iOS and Web platforms by using Ionic and Azure web services</p><br/>
+          <p>
+            Worked with a major financial client to develop a new hybrid web app
+            to accelerate business to delivery speed of Application to Android,
+            iOS and Web platforms by using Ionic and Azure web services
+          </p>
+          <br />
           As a part of core Foundation team responsible for:
           <ul>
             <li>
-            Creation and maintenance of foundational components such as authentication (OAuth2 via AD B2C),
-            Biometrics
+              Creation and maintenance of foundational components such as
+              authentication (OAuth2 via AD B2C), Biometrics
             </li>
             <li>
-            Implementation of Native functionalities like Push Notifications (via Azure Notification Hub), Deep Linking,
-            App share
+              Implementation of Native functionalities like Push Notifications
+              (via Azure Notification Hub), Deep Linking, App share
             </li>
             <li>
-            Integrate application with Kochava platform to drive app install attributions and analytics collection
+              Integrate application with Kochava platform to drive app install
+              attributions and analytics collection
             </li>
             <li>
-            Implement omnichannel messaging platform via Braze to drive Email, Push notification, In-App messaging
-            and SMS campaigns based on user behaviour for enhancing user interaction with the app
+              Implement omnichannel messaging platform via Braze to drive Email,
+              Push notification, In-App messaging and SMS campaigns based on
+              user behaviour for enhancing user interaction with the app
             </li>
             <li>
-            Working closely with Product Owner’s to refine requirements, Architects to create system design and
-            DevOps to update pipelines according to changing app needs.
+              Working closely with Product Owner’s to refine requirements,
+              Architects to create system design and DevOps to update pipelines
+              according to changing app needs.
             </li>
             <li>
-              Technologies: Ionic, Capacitor, ReactJS, Kochava, Braze, Biometrics, Sass, MongoDB, reCAPTCHA v3, SFTP,
-              Azure cloud services (Cosmos/APIM/AD B2C/App Insights/Storage/Notification Hub/etc)
+              Technologies: Ionic, Capacitor, ReactJS, Kochava, Braze,
+              Biometrics, Sass, MongoDB, reCAPTCHA v3, SFTP, Azure cloud
+              services (Cosmos/APIM/AD B2C/App Insights/Storage/Notification
+              Hub/etc)
             </li>
           </ul>
           <br />
@@ -102,7 +123,9 @@ const StaticResume = () => {
           <br />
           <h2>Roles: </h2>
           <p className={styles.roleName}>
-            <em>Specialist – Application Architecture, July 2019 to April 2021</em>{" "}
+            <em>
+              Specialist – Application Architecture, July 2019 to April 2021
+            </em>{" "}
           </p>
           As a member of architecture team I&apos;m responsible for
           <ul>
